705f2dac19cff135f07ef66814cc777c193ec926
Juan Guijarro <juanguijarroferreiro@gmail.com>
2026-06-08 19:28:32 +0200

    first commit

---
+++ src/Ch5.purs
@@ -0,0 +1,127 @@
+module Ch5 where
+
+import Data.List (List(..), (:))
+import Data.Maybe (Maybe(..))
+import Effect (Effect)
+import Effect.Console (log)
+import Prelude (Unit, (+), (-), (<), (>=), (/=), (==), show, discard, negate, otherwise, type (~>))
+import Prim.RowList (Nil)
+
+flip :: ∀ a b c. (a -> b -> c) -> b -> a -> c
+flip f x y = f y x
+
+const :: ∀ a b. a -> b -> a
+const x _ = x
+
+apply :: ∀ a b. (a -> b) -> a -> b
+apply f x = f x
+infixr 0 apply as $
+
+applyFlipped :: ∀ a b. a -> (a -> b) -> b
+applyFlipped x f = f x
+infixl 1 applyFlipped as #
+
+singleton :: ∀ a. a -> List a
+singleton x = x : Nil
+
+null :: ∀ a. List a -> Boolean
+null Nil = true
+null _ = false
+
+snoc :: ∀ a. List a -> a -> List a
+snoc Nil y = singleton y 
+snoc (x : xs) y = x : snoc xs y 
+
+length :: ∀ a. List a -> Int
+length l = go 0 l where
+  go :: Int -> List a -> Int
+  go acc Nil = acc
+  go acc (_ : xs) = go (acc + 1) xs
+
+head :: ∀ a. List a -> Maybe a
+head Nil = Nothing
+head (x : _) = Just x
+
+tail :: ∀ a. List a -> Maybe (List a)
+tail Nil = Nothing
+tail (_ : xs) = Just xs
+
+last :: ∀ a. List a -> Maybe a
+last Nil = Nothing
+last (x : Nil) = Just x
+last (_ : xs) = last xs
+
+init :: ∀ a. List a -> Maybe (List a)
+init Nil = Nothing
+init (_ : Nil) = Just Nil
+init l = Just $ go l where
+  go Nil = Nil
+  go (_ : Nil) = Nil
+  go (x : xs) = x : go xs
+
+uncons :: ∀ a. List a -> Maybe {head :: a, tail :: List a}
+uncons Nil = Nothing
+uncons (x : xs) = Just {head: x, tail: xs}
+
+index :: ∀ a. List a -> Int -> Maybe a
+index Nil _ = Nothing
+index _ n | n < 0 = Nothing
+index (x : _) 0 = Just x
+index (_ : xs) n = index xs (n-1)
+infixl 8 index as !!
+
+findIndex :: ∀ a. (a -> Boolean) -> List a -> Maybe Int
+findIndex _ Nil = Nothing
+findIndex pred l = go l 0 where
+  go Nil _ = Nothing 
+  go (x: xs) i = if pred x then Just i else go xs (i+1)
+
+findLastIndex :: ∀ a. (a -> Boolean) -> List a -> Maybe Int
+findLastIndex _ Nil = Nothing
+findLastIndex pred l = go l 0 Nothing where
+  go :: List a -> Int -> Maybe Int -> Maybe Int
+  go Nil _ candidate = candidate 
+  go (x : xs) i candidate = go xs (i+1) (if pred x then Just i else candidate)
+
+reverse :: List ~> List
+reverse Nil = Nil
+reverse l = go l Nil where
+  go Nil rl = rl
+  go (x :xs) rl = go xs (x : rl)
+
+
+
+test :: Effect Unit
+test = do
+  log $ show $ flip const 1 2
+  flip const 1 2 # show # log
+  log $ show $ singleton "xyz"
+  log $ show $ null Nil
+  log $ show $ null ("abc" : Nil)
+  log $ show $ snoc (1 : 2 : Nil) 3
+  log $ show $ length $ 1 : 2 : 3 : Nil
+  log $ show $ head (Nil :: List Unit) 
+  log $ show $ head ("abc" : "123" : Nil)
+  log $ show $ tail (Nil :: List Unit)
+  log $ show $ tail ("abc" : "123" : Nil)
+  log $ show $ (last Nil :: Maybe Unit)
+  log $ show $ last ("a" : "b" : "c" : Nil)
+  log $ show $ init (Nil :: List Unit)
+  log $ show $ init (1 : Nil)
+  log $ show $ init (1 : 2 : Nil)
+  log $ show $ init (1 : 2 : 3 : Nil)
+  log $ show $ uncons (1 : 2 : 3 : Nil)
+  log $ show $ index (1 : Nil) 4
+  log $ show $ index (1 : 2 : 3 : Nil) 1
+  log $ show $ index (Nil :: List Unit) 0
+  log $ show $ index (1 : 2 : 3 : Nil) (-99)
+  log $ show $ (1 : 2 : 3 : Nil) !! 1
+  log $ show $ findIndex (_ >= 2) (1 : 2 : 3 : Nil)
+  log $ show $ findIndex (_ >= 99) (1 : 2 : 3 : Nil)
+  log $ show $ findIndex (10 /= _) (Nil :: List Int)
+  log $ show $ findLastIndex (_ == 10) (Nil :: List Int)
+  log $ show $ findLastIndex (_ == 10) (10 : 5 : 10 : -1 : 2 : 10 : Nil)
+  log $ show $ findLastIndex (_ == 10) (11 : 12 : Nil)
+  log $ show $ reverse (10 : 20 : 30 : Nil)
+  log $ show $
+concat ((1 : 2 : 3 : Nil) : (4 : 5 : Nil) : (6 : Nil) : (Nil) : Nil)
